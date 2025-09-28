3b7b36c490206db423163bdeb4663171ff93b551
Praneeth <praneethg807@gmail.com>
2025-09-28 06:53:53 +0530

    Update apiPath.js

--- src/data/apiPath.js
+++ src/data/apiPath.js
@@ -2,5 +2,5 @@
 
 // const API_URL = "http://localhost:4000";
 
-const API_URL = "https://backend-student-attendence-managment.onrender.com";
+const API_URL = "https://student-attendance-api-6pju.onrender.com/";
 export default API_URL;
